3df569266143ad8ba371989b5d11ae2ef8b0d3c4
Your Name <you@example.com>
2022-12-10 01:31:00 +0530

    clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, setCtime] = useState(time);
+
+  const UptadeTime = () => {
+
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+
+  }
+
+  setInterval(UptadeTime, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <h1>{ctime}</h1>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export { App }
